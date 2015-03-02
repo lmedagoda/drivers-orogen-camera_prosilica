2a040b190bf164357b903ccf15b94f9933d4fa02
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2015-03-02 16:10:41 +0100

    run script to have the sync out modes property values to work with camera prosilica

--- scripts/run.rb
+++ scripts/run.rb
@@ -6,7 +6,7 @@ require 'vizkit'
 Orocos.initialize
 Orocos.run 'camera_prosilica::Task' => 'camera_a' do
     camera = Orocos::TaskContext.get 'camera_a'
-    camera.camera_id = '30054'
+    camera.camera_id = '148123'
     camera.binning_x = 1
     camera.binning_y = 1
     camera.region_x = 0
@@ -20,6 +20,8 @@ Orocos.run 'camera_prosilica::Task' => 'camera_a' do
     camera.fps = 10
     camera.gain = 6
     camera.gain_mode_auto = 0
+    camera.sync_out1_mode = "FrameTriggerReady"
+    camera.sync_out2_mode = "FrameTriggerReady"
  #   camera.camera_format = :MODE_RGB
     camera.camera_format = :MODE_GRAYSCALE
     camera.output_format = :MODE_GRAYSCALE
